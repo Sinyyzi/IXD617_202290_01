c3dc0c212541a6227000b9c4e6b385aff6b9dfd5
沈心仪 <shenxinyi@xinyiMacBook-Pro.local>
2022-11-08 23:25:39 -0800

    redo sign in page

---
+++ src/js/app.js
@@ -0,0 +1,77 @@
+import { AnimalProfilePage, ListPage, RecentPage, UserProfilePage } from "./routes.js";
+import { checkSigninForm, checkUserId } from "./signin.js";
+
+// Document Ready
+$(() => {
+
+    checkUserId();
+
+    $(document)
+
+    .on("pagecontainerbeforeshow", function(event, ui) {
+        
+        /* PAGE ROUTES */
+        switch(ui.toPage[0].id) {
+            case "recent-page": RecentPage(); break;
+            case "list-page": ListPage(); break;
+            case "user-profile-page": UserProfilePage(); break;
+            case "animal-profile-page": AnimalProfilePage(); break;
+        }
+    })
+
+
+    // EVENT DELEGATION
+    .on("submit", "#signin-form", function(e) {
+        e.preventDefault();
+        checkSigninForm();
+    })
+
+    .on("click", ".js-logout", function(e) {
+        sessionStorage.removeItem("userId");
+        checkUserId();
+    })
+
+
+    .on("click", ".animal-jump", function(e) {
+        let id = $(this).data("id");
+
+        sessionStorage.animalId = id;
+    })
+    .on("click", ".location-jump", function(e) {
+        let id = $(this).data("id");
+
+        sessionStorage.locationId = id;
+    })
+
+
+    .on("click", ".nav-link", function(e) {
+        let id = $(this).index();
+        $(this).parent().next().children().eq(id)
+            .addClass("active")
+            .siblings().removeClass("active");
+        $(this).addClass("active")
+            .siblings().removeClass("active");
+    })
+
+
+
+    // ACTIVATE TOOLS
+    .on("click", "[data-activate]", function(e) {
+        const target = $(this).data("activate");
+        $(target).addClass("active");
+    })
+    .on("click", "[data-deactivate]", function(e) {
+        const target = $(this).data("deactivate");
+        $(target).removeClass("active");
+    })
+    .on("click", "[data-toggle]", function(e) {
+        const target = $(this).data("toggle");
+        $(target).toggleClass("active");
+    })
+    .on("click", "[data-activateone]", function(e) {
+        const target = $(this).data("activateone");
+        $(target).toggleClass("active")
+            .siblings().removeClass("active");
+    })
+});
+
